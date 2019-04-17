0890c3dcfe307eb783ca972ddd8b8c8f9d12dc1b
Everaldo Lima <jemlima@gmail.com>
2019-04-17 10:59:59 -0300

    Adicionando Cors no package.json

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,10 @@ const routes = express.Router();
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
+app.get('/', function(req, res){
+    res.send('OK');
+});
+
 routes.post('/boxes', BoxController.store);
 routes.get('/boxes/:id', BoxController.show);
 routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
